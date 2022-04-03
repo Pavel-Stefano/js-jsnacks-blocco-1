6a140880c017567eeaca51104f662216c43238b6
Pavel-Stefano <pavelstefanominuti@gmail.com>
2022-04-03 09:24:35 +0200

    snack-3

--- snack-3/script.js
+++ snack-3/script.js
@@ -3,7 +3,25 @@
 //  compongono il numero.
 
 let numberUtente = prompt('scegli un numero di 4 cifre');
+let contaNum;
 
-if (
-    numberUtente.length !== 4 
-)
+if (numberUtente.length == 4) {
+    console.log(numberUtente + '_' + numberUtente.length);
+
+    contaNum = numberUtente.split('');
+    let somma = 0;
+    let index = 0;
+
+while (index < contaNum.length) {
+    console.log('index: ' , index);
+    console.log('contanum ' ,contaNum[index]);
+    
+    somma += parseInt(contaNum[index]) ;
+    index ++ ;
+
+    console.log('somma' , somma);
+
+    }
+}
+
+  
